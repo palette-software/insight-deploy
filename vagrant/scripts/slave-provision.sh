c7ca7df3adf7e82938c37ec2efe8f0c4778bd547
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-06 16:28:35 +0200

    nginx + supervisor deployment, local vagrant box creation

---
+++ vagrant/scripts/slave-provision.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## RHEL/CentOS 6 64-Bit ##
+
+sudo yum install wget python -y
+
+# Add the EPEL repo
+wget --no-check-certificate http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo rpm -ivh epel-release-6-8.noarch.rpm
+
+# replace https with http in /etc/yum.repos.d/epel.repo 
+sudo perl -i -pe 's/https/http/g' /etc/yum.repos.d/epel.repo 
+
+# install ansible
+sudo yum install ansible -y
+
+
+# Copy the deployment key over
+cp /provisioning/vagrant/keys/insight-deploy-key /home/vagrant/insight-deploy-key
