7f2d8c3c072a4aac061e217a3086bd8f10e2b3ee
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-06 16:30:36 +0200

    removed build.sh containing leaked AWS keys (which have been deactivated since)

--- packer/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PACKER_DIR=`pwd`/$0/..
-
-packer build \
-    -var 'aws_access_key=AKIAJDBQ6YZYUQNFIIXA' \
-    -var 'aws_secret_key=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' \
-    ${PACKER_DIR}/redhat-72-us-east-1.json
