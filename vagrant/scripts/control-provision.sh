3a19ff24f4e283349b51c2fb11b28f9237f33cfd
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-08 20:40:24 +0200

    modularized ansible bits, added golang & palette developer tools, started supervisor 2 migration

--- vagrant/scripts/control-provision.sh
+++ vagrant/scripts/control-provision.sh
@@ -8,12 +8,13 @@ sudo yum install wget python -y
 wget --no-check-certificate http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -ivh epel-release-6-8.noarch.rpm
 
-# replace https with http in /etc/yum.repos.d/epel.repo 
-sudo perl -i -pe 's/https/http/g' /etc/yum.repos.d/epel.repo 
+# replace https with http in /etc/yum.repos.d/epel.repo
+sudo perl -i -pe 's/https/http/g' /etc/yum.repos.d/epel.repo
 
 # install ansible
 sudo yum install ansible -y
 
 
 # Copy the deployment key over
-cp /provisioning/vagrant/keys/insight-deploy-key /home/vagrant/insight-deploy-key
+cp /provisioning/vagrant/keys/insight-deploy-key /home/vagrant/.ssh/insight-deploy-key
+chmod 600 /home/vagrant/.ssh/insight-deploy-key
