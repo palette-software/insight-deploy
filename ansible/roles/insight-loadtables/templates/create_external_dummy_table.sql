430e41e80e7894450fcc022ac4ab1e43ea30660d
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-27 17:55:00 +0200

    added dummy external table creator to install-reporting

---
+++ ansible/roles/insight-loadtables/templates/create_external_dummy_table.sql
@@ -0,0 +1,14 @@
+-- Create dummy table
+CREATE READABLE EXTERNAL TABLE {{ insight_schema_name }}.dummy_to_create_ext_error_table
+(
+    dummy int
+)
+LOCATION (
+    'gpfdist://localhost:18001/*/dummy-*.csv.gz'
+)
+FORMAT 'TEXT' (delimiter '' null '\N' escape '\' header)
+ENCODING 'UTF8'
+LOG ERRORS INTO "{{ insight_schema_name }}"."ext_error_table" SEGMENT REJECT LIMIT 1000 ROWS;
+
+-- Drop the table
+drop external table {{ insight_schema_name }}.dummy_to_create_ext_error_table;
