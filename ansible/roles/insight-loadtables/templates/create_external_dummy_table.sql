dd7636bab7b90226259b2789eba22d4adb3d0afd
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-30 15:28:07 +0200

    fix: full reporting install

--- ansible/roles/insight-loadtables/templates/create_external_dummy_table.sql
+++ ansible/roles/insight-loadtables/templates/create_external_dummy_table.sql
@@ -8,7 +8,7 @@ LOCATION (
 )
 FORMAT 'TEXT' (delimiter '' null '\N' escape '\' header)
 ENCODING 'UTF8'
-LOG ERRORS INTO "{{ insight_schema_name }}"."ext_error_table" SEGMENT REJECT LIMIT 1000 ROWS;
+LOG ERRORS INTO "{{ insight_schema_name }}"."{{ ext_errors_table_name }}" SEGMENT REJECT LIMIT 1000 ROWS;
 
 -- Drop the table
 drop external table {{ insight_schema_name }}.dummy_to_create_ext_error_table;
