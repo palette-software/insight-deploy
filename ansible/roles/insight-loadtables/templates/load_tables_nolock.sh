efac471ebc3a6d8afe7d45e7758136a44514c69d
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-31 14:09:34 +0200

    hostname, nolock, 9.2.4 * added loadtables-nolock * added missing tableau 9.2.4 metadata * added hostname setting

---
+++ ansible/roles/insight-loadtables/templates/load_tables_nolock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Fail if there are errors
+set -e
+
+{{ insight_talend_location }}/Palette_Insight_Init_ImportTable/Palette_Insight_Init_ImportTable_run.sh \
+  --context_param storage_path=/data/insight-server/uploads/{{ cluster_name }}
