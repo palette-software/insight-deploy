6ae9ccb5aa994218a83cd6caabad91c087df76af
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-06-30 09:29:51 +0000

    fix: revert table names because they were already changed

--- ansible/roles/insight-loadtables/templates/vacuum.sh
+++ ansible/roles/insight-loadtables/templates/vacuum.sh
@@ -4,7 +4,6 @@ psql -d palette >/var/log/vacuum/vacuum.log 2>&1 <<EOF
 VACUUM ANALYZE {{ insight_schema_name }}.p_serverlogs;
 VACUUM ANALYZE {{ insight_schema_name }}.p_cpu_usage;
 VACUUM ANALYZE {{ insight_schema_name }}.p_cpu_usage_report;
-
-VACUUM ANALYZE {{ insight_schema_name }}.p_interactor_session_agg_cpu_usage;
+VACUUM ANALYZE {{ insight_schema_name }}.p_interactor_session;
 VACUUM ANALYZE {{ insight_schema_name }}.p_cpu_usage_agg_report;
 EOF
