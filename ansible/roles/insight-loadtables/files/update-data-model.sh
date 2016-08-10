02a8f51ebd873442cea3e6cf25a994a1b88a9f93
Peter Bastyi <baspeti@gmail.com>
2016-08-10 16:13:14 +0200

    Bash script for updating data model

---
+++ ansible/roles/insight-loadtables/files/update-data-model.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+data_model_version=$1
+if [[ -n "$data_model_version" ]]; then
+    echo "Requested data model version: $data_model_version"
+else
+    echo "No data model version specified. Trying to install the latest one."
+fi
+
+# We need to make sure we don't get our latest package from yum cache.
+sudo yum clean all
+sudo yum install -y palette-insight-reporting
+
+# Find a place which is available both for centos and gpadmin user
+cp -f ./gpadmin-install-data-model.sh /tmp/
+sudo su -c "/tmp/gpadmin-install-data-model.sh $data_model_version" gpadmin
+rm /tmp/gpadmin-install-data-model.sh
