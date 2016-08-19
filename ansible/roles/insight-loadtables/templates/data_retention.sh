42ff41f5ae60650a57db7beccb283fa6bfc93888
Gergely Kovacs <kovacsg@starschema.net>
2016-08-19 17:11:27 +0200

    Retention for http_requests.

--- ansible/roles/insight-loadtables/templates/data_retention.sh
+++ ansible/roles/insight-loadtables/templates/data_retention.sh
@@ -22,8 +22,40 @@ psql -tc "select
 				partitionname <> '10010101'
 			) a
 		where
-			rn > 8	
+			rn > 15
 		order by 1
 	" $DBNAME | psql -a $DBNAME >> $LOGFILE 2>&1
 
 echo "End drop old partitions. " + $(date) >> $LOGFILE
+
+
+delete from palette.http_requests as t
+using
+	(
+	
+	with t_dates 
+	as
+	(
+	select 
+		distinct created_at::date as created_at_date
+	from 
+		palette.http_requests
+	)
+	
+	select
+		created_at_date
+	from	
+		(
+		select 
+			d.created_at_date,
+			row_number() over (order by d.created_at_date desc) as rn
+		from 
+			t_dates d
+		) a	
+	where
+		rn > 15
+		) s
+where
+	t.created_at::date = s.created_at_date
+
+	
