d34377f339cafe5275fc42946bc52d3d0a8d3857
Szilard Huber <szilard.huber@gmail.com>
2016-10-07 10:58:48 +0200

    Change cluster_name param to hardcoded palette.

--- ansible/roles/insight-loadtables/templates/run_reporting.sh
+++ ansible/roles/insight-loadtables/templates/run_reporting.sh
@@ -7,4 +7,4 @@ LOCKFILE=/tmp/PI_Reporting_prod.flock
 
 flock -n ${LOCKFILE} \
   {{ insight_talend_location }}/Palette_Insight_Init_Reporting/Palette_Insight_Init_Reporting_run.sh \
-    --context_param storage_path=/data/insight-server/uploads/{{ cluster_name }}
+    --context_param storage_path=/data/insight-server/uploads/palette
